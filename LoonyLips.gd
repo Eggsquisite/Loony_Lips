425ae6357771d95278ca9d1aaa83d9579b523f36
Marc Sevilleja <marc@sevilayha.com>
2020-07-06 01:45:07 -0700

    Basics of game finished

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,22 +1,46 @@
 extends Control
 
+var player_words = []
+var prompts = ["a name", "a noun", "an emotion", "a noun"]
+var story = "There once was a man named %s who everyone in his village thought was a %s. Filled with %s, he left the village in search of the meaning of %s."
+
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var DisplayText = $VBoxContainer/DisplayText
 
 func _ready():
-	var prompts = ["Jacob", "murderer", "anger", "tacos"]
-	var story = "There once was a man named %s who everyone in his village thought was a %s. Filled with %s, he left the village in search of the meaning of %s"
-	$VBoxContainer/DisplayText.text = story % prompts
+	DisplayText.text = "Welcome to Loony Lips! We're going to try our hand at storytelling! \n\n"
+	check_player_words_length()
 
 
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
-
+	add_to_player_words()
+	
 
 func _on_TextureButton_pressed():
-	update_DisplayText($VBoxContainer/HBoxContainer/PlayerText.text)
+	add_to_player_words()
 
 
-func update_DisplayText(words):
-	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
 
 
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
+
+
+func tell_story():
+	DisplayText.text = story % player_words
+
+
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
