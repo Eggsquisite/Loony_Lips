dff6bf73f63c098b6c8ed8d5db9c89c5499b78d7
Marc Sevilleja <marc@sevilayha.com>
2020-07-06 00:56:37 -0700

    Update display text based on player input

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -2,8 +2,15 @@ extends Control
 
 
 func _ready():
-	var prompts = ["Yann", "slut", "lust", "tacos"]
+	var prompts = ["Jacob", "murderer", "anger", "tacos"]
 	var story = "There once was a man named %s who everyone in his village thought was a %s. Filled with %s, he left the village in search of the meaning of %s"
-	print(story % prompts)
-	
-	$DisplayText.text = "The new text"
+	$VBoxContainer/DisplayText.text = story % prompts
+
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/PlayerText.clear()
