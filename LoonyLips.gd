171a0ac9f6fd221e0bc79c35492db8087d30fa5b
Marc Sevilleja <marc@sevilayha.com>
2020-07-07 05:22:10 -0700

    free nodes from memory, get root node, and reload scene for a "full game"

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -5,19 +5,25 @@ var prompts = ["a name", "a noun", "an emotion", "a noun"]
 var story = "There once was a man named %s who everyone in his village thought was a %s. Filled with %s, he left the village in search of the meaning of %s."
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var ConfirmText = $VBoxContainer/HBoxContainer/ConfirmText
 onready var DisplayText = $VBoxContainer/DisplayText
 
 func _ready():
 	DisplayText.text = "Welcome to Loony Lips! We're going to try our hand at storytelling! \n\n"
+	ConfirmText.text = "ok"
 	check_player_words_length()
 
 
-func _on_PlayerText_text_entered(new_text):
+func _on_PlayerText_text_entered(_new_text):
 	add_to_player_words()
-	
+
 
 func _on_TextureButton_pressed():
-	add_to_player_words()
+	if is_story_done():
+		# restart game when pressing button and story is done
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
 
 
 func add_to_player_words():
@@ -33,14 +39,22 @@ func is_story_done():
 
 func check_player_words_length():
 	if is_story_done():
-		tell_story()
+		end_input()
 	else:
 		prompt_player()
+
+
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+
+
+func end_input():
+	PlayerText.queue_free()
+	ConfirmText.text = "again!"
+	tell_story()
 
 
 func tell_story():
 	DisplayText.text = story % player_words
 
 
-func prompt_player():
-	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
