7733e068e7b193039d8f358bf44927b7623e5338
Marc Sevilleja <marc@sevilayha.com>
2020-07-07 20:33:56 -0700

    Revert "Added dictionary (key : value) templates to randomize stories"
    This reverts commit 639b43e759b1e696aae159e39ecc40b26dca6fc0.

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,37 +1,17 @@
 extends Control
 
 var player_words = []
-var template = [
-		{ # new dictionary
-		"story" : "There once was a man named %s who everyone in his village thought was a %s. Filled with %s, he left the village in search of the meaning of %s.",
-		"prompts" : ["a name", "a noun", "an emotion", "a noun"]
-		},
-		{ # new dictionary
-		"story" : "A girl's %s was %s and she went in search of %s. She was %s on her journey and lost everything",
-		"prompts" : ["a noun", "a past tense verb", "a noun", "an adjective"]
-		},
-		{ # new dictionary
-		"story" : "A woman and her %s were stranded in %s when attacked by %s animals. They ran to %s to seek shelter.",
-		"prompts" : ["a noun", "a place", "an adjective", "a place"],
-		},
-		]
-var current_story
+var prompts = ["a name", "a noun", "an emotion", "a noun"]
+var story = "There once was a man named %s who everyone in his village thought was a %s. Filled with %s, he left the village in search of the meaning of %s."
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var ConfirmText = $VBoxContainer/HBoxContainer/ConfirmText
 onready var DisplayText = $VBoxContainer/DisplayText
 
 func _ready():
-	set_current_story()
 	DisplayText.text = "Welcome to Loony Lips! We're going to try our hand at storytelling! \n\n"
 	ConfirmText.text = "ok"
 	check_player_words_length()
-	PlayerText.grab_focus()
-
-
-func set_current_story():
-	randomize()
-	current_story = template[randi() % template.size()]
 
 
 func _on_PlayerText_text_entered(_new_text):
@@ -54,7 +34,7 @@ func add_to_player_words():
 
 
 func is_story_done():
-	return player_words.size() == current_story.prompts.size()
+	return player_words.size() == prompts.size()
 
 
 func check_player_words_length():
@@ -65,7 +45,7 @@ func check_player_words_length():
 
 
 func prompt_player():
-	DisplayText.text += "May I have " + current_story.prompts[player_words.size()] + " please?"
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
 
 
 func end_input():
@@ -75,6 +55,6 @@ func end_input():
 
 
 func tell_story():
-	DisplayText.text = current_story.story % player_words
+	DisplayText.text = story % player_words
 
 
